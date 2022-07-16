e824848a1a8e796162d804e260e12d2857e2a4a4
Masaya <masaya.sasaki.99@gmail.com>
2022-07-16 22:12:24 +0900

    first commit

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,34 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+// Import the OpenZepellin ERC721
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+// Inherit the Ope
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds; 
+
+    constructor() ERC721("MyNFT", "NFT") {
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current(); 
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId,tokenURI);
+        
+        return newItemId; 
+    }
+
+}
